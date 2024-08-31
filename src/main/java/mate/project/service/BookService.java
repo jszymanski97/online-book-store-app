8fea99e15152627343f2f06117c7e2a9b2fa9fcc
Jakub <jszymanski97@gmail.com>
2024-08-31 16:06:50 +0100

    BookController.java - created with supporting BookDto.java and CreateBookRequestDto.java classes MapperConfig.java - implemented mapstruct to ease the class conversion EntityNotFoundException.java - exception created for error message clarity

--- src/main/java/mate/project/service/BookService.java
+++ src/main/java/mate/project/service/BookService.java
@@ -1,10 +1,13 @@
 package mate.project.service;
 
 import java.util.List;
-import mate.project.model.Book;
+import mate.project.dto.BookDto;
+import mate.project.dto.CreateBookRequestDto;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto createBookRequestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto getBookById(Long id);
 }
