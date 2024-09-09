11443c0de0d94604e0f0af39df33152b4301039b
Jakub <jszymanski97@gmail.com>
2024-09-09 22:18:36 +0100

    CustomGlobalExceptionHandler implemented for returning clear error messages and also returning custom exception for invalid id CreateBookRequestDto.java - Validation annotations added

--- src/main/java/mate/project/controller/BookController.java
+++ src/main/java/mate/project/controller/BookController.java
@@ -1,5 +1,6 @@
 package mate.project.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.project.dto.BookDto;
@@ -35,7 +36,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
