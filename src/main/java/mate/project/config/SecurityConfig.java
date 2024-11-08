22cbbddadaf9cbeb871de091adcacce79a619c31
Jakub <jszymanski97@gmail.com>
2024-11-08 22:50:45 +0000

    Basic Authentication implemented in the project, Role Entity created to associate Users with specific role types Now User and Admin role have access to different endpoints ie Admin can create, delete, amend books and User cannot New liquibase change sets added to create roles and user_roles tables. Also, changesets created to implement data in to users, roles and users_roles tables

---
+++ src/main/java/mate/project/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package mate.project.config;
+
+import lombok.RequiredArgsConstructor;
+import mate.project.security.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableMethodSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/auth/**", "/error", "/swagger-ui/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(customUserDetailsService)
+                .build();
+    }
+}
