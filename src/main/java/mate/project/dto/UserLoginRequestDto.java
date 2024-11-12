d1d391950eaa26fea04bb07c04fb6253039e94c5
Jakub <jszymanski97@gmail.com>
2024-11-12 19:17:02 +0000

    Removing redundant pom.xml lines UserLoginRequestDto NotNull updated to NotBlank annotation

--- src/main/java/mate/project/dto/UserLoginRequestDto.java
+++ src/main/java/mate/project/dto/UserLoginRequestDto.java
@@ -1,15 +1,15 @@
 package mate.project.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginRequestDto(
-        @NotEmpty
+        @NotBlank
         @Email
         @Size(min = 8, max = 255)
         String email,
-        @NotEmpty
+        @NotBlank
         @Size(min = 8, max = 255)
         String password
 ) {
